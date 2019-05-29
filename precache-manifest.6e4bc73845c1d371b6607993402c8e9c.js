b90954cf42f9800dc24a0cb0551000eb5874accf
Rahul Banerjee <40443639+rahulbanerjee26@users.noreply.github.com>
2019-05-29 04:55:21 +0300

    Updates

---
+++ precache-manifest.6e4bc73845c1d371b6607993402c8e9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb2a9d1185e83ed5554116a55cf8bb2",
+    "url": "/news_dash/index.html"
+  },
+  {
+    "revision": "db7cc30535087fc696e0",
+    "url": "/news_dash/static/css/main.76dfede3.chunk.css"
+  },
+  {
+    "revision": "a3d29448eec3afd90b8c",
+    "url": "/news_dash/static/js/2.424c27cb.chunk.js"
+  },
+  {
+    "revision": "db7cc30535087fc696e0",
+    "url": "/news_dash/static/js/main.eabca05a.chunk.js"
+  },
+  {
+    "revision": "bdce9907fd092530d077",
+    "url": "/news_dash/static/js/runtime~main.376d8bd6.js"
+  }
+]);
